5e86ae3994d61fabb8632fda35290b0dba90f849
GabrielJacobina <sgabrieljacobina@gmail.com>
2021-01-28 15:12:18 -0300

    Customizing exceptions

---
+++ backend/src/main/java/com/techlead/gerenciamentodelivros/handler/RestExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.techlead.gerenciamentodelivros.handler;
+
+import com.techlead.gerenciamentodelivros.exception.CustomException;
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Bean
+    public ErrorAttributes errorAttributes() {
+        return new DefaultErrorAttributes() {
+            @Override
+            public Map<String, Object> getErrorAttributes(WebRequest webRequest, boolean includeStackTrace) {
+                Map<String, Object> errorAttributes = super.getErrorAttributes(webRequest, includeStackTrace);
+                errorAttributes.remove("exception");
+                return errorAttributes;
+            }
+        };
+    }
+
+    @ExceptionHandler(CustomException.class)
+    public void handleCustomException(HttpServletResponse res, CustomException ex) throws IOException {
+        res.sendError(ex.getHttpStatus().value(), ex.getMessage());
+    }
+}
